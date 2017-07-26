6947cd44badc98b8d471a2530070a25a3de0f207
kamiloc <kamilo.sniper@gmail.com>
2017-07-26 04:22:36 -0500

    fix server for develop

--- server.js
+++ server.js
@@ -4,18 +4,26 @@ var PORT = process.env.PORT || 5000;
 
 var express = require('express');
 var app = express();
-var folder = __dirname + '/public';
+var folder = __dirname + '/public/';
 
 // expose public folder as static assets
-app.use(express.static(folder));
+app.use('/domicilios-test', express.static(folder));
 
 // add our app routes
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
 
-    res.sendFile(folder + '/index.html');
+    if (req.url === '/') {
+        req.url += 'domicilios-test/';
+    }
+    if (req.url === '/domicilios-test/') {
+        res.sendFile(folder + 'index.html');
+    } else {
+        res.status(404).sendFile(folder + '404.html');
+    }
+
 });
 
 const server = app.listen(PORT, function () {
     // console.log();
-    console.log('\x1b[36m%s\x1b[2m', 'The Aplication is now listening at http://localhost/'+PORT);
+    console.log('\x1b[36m%s\x1b[2m', 'The Aplication is now listening at http://localhost/' + PORT);
 });
